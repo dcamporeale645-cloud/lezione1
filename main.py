c32d8813399a051cb25bfd4d4dff7799b2e8a2f6
Diego <dcamporeale645@gmail.com>
2026-05-20 17:29:15 +0200

    Riassegnazione della Variabile

--- main.py
+++ main.py
@@ -2,3 +2,46 @@ strumento_musicale: str = "basso"
 strumento_musicale = []
 print (strumento_musicale)
 
+
+# Riassegnazione della variabile in Python
+# Variabile Booleana
+
+online = True
+print (online)
+
+# Riassegnazione del Valore
+# Booleana → Booleana
+online = True
+online = False
+print (online)
+
+# Stringa → Lista
+accesso: str = "negato"
+accesso = [1,2,3]
+print (accesso)
+
+# Lista → Stringa
+band = ["basso", "batteria", "chitarra"]
+band: str = "rock band"
+print (band)
+
+# Tuple → Stringa
+coordinate: tuple = (10,20)
+print (coordinate)
+
+coordinate: tuple = (10,20)
+coordinate: str = "punto A"
+print (coordinate)
+
+# Stessa Variabile, Tipi diversi... DA COMPLETARE!!
+dato = "ciao"
+dato = 100
+dato = True
+dato = ["Python", "Java"]
+print (dato)
+
+dato: str = "ciao"
+dato: int = 100
+dato: bool = True
+dato: list = []
+print (dato)
